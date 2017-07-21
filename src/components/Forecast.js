34a1b708d2e5c07f5f9c9f888f7ece5631a01fc0
Jonathan Grim <jonjongrim@gmail.com>
2017-07-21 11:43:27 -0400

    Add loading component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loading from './Loading';
 const queryString = require('query-string');
 
 const weatherKey = '348c880899f24360d8ade9d6e84acc09';
@@ -13,18 +14,27 @@ class Forecast extends Component {
   }
 
   componentDidMount() {
-    const { searchTerm = '' } = queryString.parse(this.props.location.search);
+    console.log(this.props.location.search);
+    const { city: searchTerm = '' } = queryString.parse(this.props.location.search);
+    console.log(searchTerm);
     this.setState(() => {
       return {
         searchTerm: searchTerm
       };
-    });
+    }, this.getAllWeather);
+  }
+
+  getAllWeather() {
+    console.log(this);
     axios
       .all([this.getCurrentWeather(), this.getForecastWeather()])
       .then(
         axios.spread((cur, forecast) => {
           console.log(cur);
           console.log(forecast);
+          this.setState(() => {
+            return { current: cur.data, forecast: forecast.data };
+          });
         })
       )
       .catch(err => {
@@ -46,9 +56,15 @@ class Forecast extends Component {
   }
 
   render() {
+    const current = this.state.current;
+    const forecast = this.state.forecast;
+
     return (
       <div>
         <h1>Forecast</h1>
+        {!(current && forecast) && <Loading text="Sticking our hand out the window" />}
+        {current && JSON.stringify(current)}
+        {forecast && JSON.stringify(forecast)}
       </div>
     );
   }
